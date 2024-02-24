11f3ba3da175fe6fd77e9380cae257807888960c
Vamsi Batta <vamsi.batta@five9.com>
2024-02-24 13:01:32 +0530

    Data Ingestion IPYNB

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("Hello")
+import yfinance as yf
+import pandas as pd
+
+stock_symbol = "TATAMOTORS.NS"
+stock_data = yf.Ticker(stock_symbol)
+live_data = stock_data.history(period='20y', interval='1d')
+df = pd.DataFrame(live_data)
+
+df.to_csv("TATAMOTORS.csv")
+print(live_data)
---
+++ src/constants/__init__.py
@@ -0,0 +1,2 @@
+DATA_INGESTION_CONFIG_KEY="data_ingestion_config"
+DATA_INGESTION_DATASET_URL="dataset_url"
