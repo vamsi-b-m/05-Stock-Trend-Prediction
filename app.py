2ad5c32f1d380ad9ba5992fac96bc4e115a19dc0
vamsi-b-m <149484204+vamsi-b-m@users.noreply.github.com>
2024-06-20 19:15:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ def result():
 
 
 if __name__ == '__main__':
-    app.run(port='0.0.0.0', port='5000', debug=True, use_reloader=False)
+    app.run(host='0.0.0.0', port='5000', debug=True, use_reloader=False)
 
 
 
